05dadddc69148b030ac9247b0b60d3a78a6010df
André Azevedo <andrev1998@hotmail.com>
2024-07-17 22:53:50 -0400

    Gerador de Senhas com JavaScript 2024

---
+++ script.js
@@ -0,0 +1,24 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const includeUppercase = document.getElementById('includeUppercase').checked;
+    const includeNumbers = document.getElementById('includeNumbers').checked;
+    const includeSymbols = document.getElementById('includeSymbols').checked;
+
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let charSet = lowerCaseChars;
+    if (includeUppercase) charSet += upperCaseChars;
+    if (includeNumbers) charSet += numberChars;
+    if (includeSymbols) charSet += symbolChars;
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charSet.length);
+        password += charSet[randomIndex];
+    }
+
+    document.getElementById('passwordDisplay').innerText = password;
+}
